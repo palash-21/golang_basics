899df60827c0e6d53a1daeeb3c648324962e4ee2
Palash Pathak <76896938+palash-21@users.noreply.github.com>
2023-06-23 15:04:08 +0530

    Add files via upload

--- 2_basicdatatypes/202_play_with_strings.go
+++ 2_basicdatatypes/202_play_with_strings.go
@@ -43,21 +43,6 @@ func main() {
 	var result2 string = string(getChar(str, index))
 	fmt.Println("The character at index", index, "is:", result2)
 
-	fmt.Println(strings.Repeat("-", 10))
-	/* rune in Go is a data type that stores codes that represent Unicode characters.
-	Unicode is actually the collection of all possible characters present in the whole world.
-	In Unicode, each of these characters is assigned a unique number called the Unicode code point.
-	This code point is what we store in a rune data type.
-
-	Each rune actually refers to only one Unicode code point, meaning one rune represents a single character.
-	Furthermore, Go does not have a char data type, so all variables initialized with a character
-	would automatically be typecasted into int32, which, in this case, represents a rune. */
-
-	// printing rune values for all chars of a string
-	var my_str string = "ABCD"
-	r_array := []rune(my_str)
-	fmt.Printf("Array of rune values for A, B, C and D: %U\n", r_array)
-
 }
 
 // function to get characters from strings using index
